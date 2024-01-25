6e50b7c926402e16b10670dee007dcc90912de1b
PaulinaMB <paulinamendozabernal@gmail.com>
2024-01-24 23:18:06 -0600

    (+) Se completó el script que permite traer el popup de cada review

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -3,22 +3,24 @@ En este caso son cada card de review */
 let previewContainer = document.querySelector('.products-preview');
 let previewBox = document.querySelectorAll('.preview');
 
-document.querySelectorAll('seccion--quinta .reviews').forEach(reviews =>{
-    reviews.onClick = () =>{
+document.querySelectorAll('.center .reviews--quinta').forEach(reviews =>{
+    reviews.addEventListener('click', () =>{
         previewContainer.style.display = 'flex';
-        let name = product.getAttribute('data-name');
+        let name = reviews.getAttribute('data-name');
         previewBox.forEach(preview =>{
             let target = preview.getAttribute('data-target');
             if(name == target){
                 preview.classList.add('active');
+            }else{
+                preview.classList.remove('active');
             }
         });
-    };
+    });
 });
 
 previewBox.forEach(close=>{
-    close.querySelector('.fa-xmark').onClick = () =>{
+    close.querySelector('.fa-xmark').addEventListener( 'click', () => {
     close.classList.remove('active');
     previewContainer.style.display = 'none';
-    };
+    });
 });
