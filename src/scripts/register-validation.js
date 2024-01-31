81282a906a0a4c90109496bf8f95755c3f681f71
carlosEsalinas <salinas_carlos@ciencias.unam.mx>
2024-01-31 14:30:22 -0600

    corrección de reguster.js y limpieza a login.html y register.html

--- src/scripts/register-validation.js
+++ src/scripts/register-validation.js
@@ -1,26 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.getElementById('register-form');
-
-//     form.addEventListener('submit', function (event) {
-//         event.preventDefault();
-//         validatePasswords();
-//     });
-
-//     function validatePasswords() {
-//         const password = document.getElementById('password').value;
-//         const againPassword = document.getElementById('again-password').value;
-
-//         if (password !== againPassword) {
-//             alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
-//         } else {
-//             form.submit(); // Envía el formulario si las contraseñas coinciden
-//         }
-//     }
-// });
-
-
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('register-form');
 
