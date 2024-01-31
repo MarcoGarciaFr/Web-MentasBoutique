b4dd196544c126ae446c014632d18ea6d39c5198
carlosEsalinas <salinas_carlos@ciencias.unam.mx>
2024-01-31 14:22:10 -0600

    Corrección de campos de validación para login.html y register.html

--- src/scripts/register-validation.js
+++ src/scripts/register-validation.js
@@ -1,19 +1,65 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.getElementById('register-form');
+
+//     form.addEventListener('submit', function (event) {
+//         event.preventDefault();
+//         validatePasswords();
+//     });
+
+//     function validatePasswords() {
+//         const password = document.getElementById('password').value;
+//         const againPassword = document.getElementById('again-password').value;
+
+//         if (password !== againPassword) {
+//             alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
+//         } else {
+//             form.submit(); // Envía el formulario si las contraseñas coinciden
+//         }
+//     }
+// });
+
+
+// script.js
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('register-form');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        validatePasswords();
+        validateForm();
     });
+
+    function validateForm() {
+        validateField('name');
+        validateField('email');
+        validatePasswords();
+    }
+
+    function validateField(fieldName) {
+        const field = document.getElementById(fieldName);
+        const invalidFeedback = field.nextElementSibling; // Siguiente elemento después del campo
+
+        if (field.value.trim() === '') {
+            invalidFeedback.style.display = 'block';
+        } else {
+            invalidFeedback.style.display = 'none';
+        }
+    }
 
     function validatePasswords() {
         const password = document.getElementById('password').value;
         const againPassword = document.getElementById('again-password').value;
 
+        const mensajeInvalidoPassword = document.getElementById('mensajeInvalidoPassword');
+        const mensajeValidoPassword = document.getElementById('mensajeValidoPassword');
+
         if (password !== againPassword) {
-            alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
+            mensajeInvalidoPassword.style.display = 'block';
+            mensajeValidoPassword.style.display = 'none';
         } else {
-            form.submit(); // Envía el formulario si las contraseñas coinciden
+            mensajeInvalidoPassword.style.display = 'none';
+            mensajeValidoPassword.style.display = 'block';
+            form.submit();
         }
     }
 });
